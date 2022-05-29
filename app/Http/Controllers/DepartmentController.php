ebaa134177754202a80e448257e447b2b44bed0f
inoue123456 <yuta1511814@gmail.com>
2022-05-29 05:49:20 +0000

    #11 fix

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Department;
+use App\User;
+
+class DepartmentController extends Controller
+{
+    public function add() {
+        return view('department.new');
+    }
+    
+    public function create(Request $request)
+    {
+        $this->validate($request, Department::$rules);
+        $department = new Department;
+        $department->fill($request->only(['department_name']));
+        //部長名からユーザー名が一致するuser_idを取得
+        $user_name = $request->user_name;
+        //dd($user_name);
+        $user = User::where('name', $user_name)->first();
+        //dd($user);
+        $department->user_id = $user->id;
+        $department->save();
+        return redirect()->back();
+    }
+    
+    public function index()
+    {
+        return view('department.index', ['departments' => Department::all()]);
+    }
+    
+    
+}
