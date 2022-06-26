eb3b223cd1015497b86b3c7546398dba2cc18761
inoue123456 <yuta1511814@gmail.com>
2022-06-26 12:47:31 +0000

    部長名表示

---
+++ resources/views/department/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-10 col-md-offset-2">
+        <h2>部署一覧</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>部署名</th>
+              <th>部長名</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($departments as $department)
+            <tr>
+              <td>{{ $department->department_name }}</th>
+              <td>{{ $department->user->name }}</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
+@endsection 
