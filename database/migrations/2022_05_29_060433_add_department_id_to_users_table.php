c8c3742f4ddfb2eaab74996aeb20819f75561aa9
inoue123456 <yuta1511814@gmail.com>
2022-06-26 13:14:07 +0000

    usersテーブルにdepartment_idカラム追加

--- database/migrations/2022_05_29_060433_add_department_id_to_users_table.php
+++ database/migrations/2022_05_29_060433_add_department_id_to_users_table.php
@@ -26,7 +26,7 @@ class AddDepartmentIdToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('department_id');
         });
     }
 }
