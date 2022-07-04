ecf282fe3cf04253565655d8b30eac1a26fe3c11
inoue123456 <yuta1511814@gmail.com>
2022-07-04 13:47:39 +0000

    パスワードの変更部分を改善

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -25,23 +25,36 @@ class EmployeeController extends Controller
         $new_password = $request->password;
         $old_password = $request->currentpassword;
         $new_password_confirm = $request->password_confirmation;
-        //dd($new_password_confirm);
-        /*$hash = $user->password;
-        dd($hash);
-        $hash = '$2y$10$gheWK87zN9nxZU4AtjH.te4/L72kHpzFQbyRDu5EK8.WFMMTRLc8q';
         
-        if (Hash::check($old_password, $user->password)) {
-            echo "一致";
-        } else {
-            echo "不一致";
+        
+        function matchCurrentPassword($old_password, $user) {
+            //return $old_password == $user->password;
+            return Hash::check($old_password, $user->password);
         }
-        /*$request->user()->fill([
-            'password' => Hash::check($old_password)
-        ])
-        $request->user()->fill([
-            'password' => Hash::make($new_password)
-        ])->save();
-        //*/
+        
+        function confirmedNewPassword($new_password, $new_password_confirm) {
+                return $new_password == $new_password_confirm;
+                //※$new_password と$new_password_confirmどちらもnullのとき登録されちゃう
+        }
+        
+        if(matchCurrentPassword($old_password, $user) && confirmedNewPassword($new_password, $new_password_confirm)) {
+            if($new_password) {
+                $request->user()->fill(['password' => Hash::make($new_password)])->save();
+                \Session::flash('changed_done','パスワードが正しく変更されました');
+            } else {
+                \Session::flash('err_msg_password_null', '新しいパスワードを入力してください。');
+            } //0のときも'新しいパスワードを入力してください。'というエラー文が表示
+        } else {
+            if(!matchCurrentPassword($old_password, $user)) {
+                \Session::flash('err_msg_currentpassword', '現在のパスワードが間違っています。');
+            }
+            if(!confirmedNewPassword($new_password, $new_password_confirm)) {
+                \Session::flash('err_msg_not_confirmed', '新しいパスワードの確認には新しいパスワードと同じパスワードを入力してください。');
+            }
+        }
+        return redirect()->back();
+        
+        /*以下を上のコードに改善
         if(!(Hash::check($old_password, $user->password))) {
            \Session::flash('err_msg_currentpassword', '現在のパスワードが間違っています。');
            return redirect()->back();
@@ -52,10 +65,8 @@ class EmployeeController extends Controller
         } elseif(Hash::check($new_password, $user->password)) {
             \Session::flash('err_msg_newpassword', '新しいパスワードが、現在のパスワードと同じです。違うパスワードを設定してください。');
             return redirect()->back();
-        } elseif($new_password == $request->password_confirmation) {
-            $request->user()->fill([
-            'password' => Hash::make($new_password)
-            ])->save();
+        } elseif($new_password == $new_password_confirm) {
+            $request->user()->fill(['password' => Hash::make($new_password)])->save();
             \Session::flash('changed_done','パスワードが正しく変更されました');
             return redirect()->back();
             //return redirect()->back()->with(['user' => $user, 'say' => 'パスワードが正しく変更されました']);
@@ -63,6 +74,7 @@ class EmployeeController extends Controller
             \Session::flash('err_msg_not_confirmed', '新しいパスワードの確認には新しいパスワードと同じパスワードを入力してください。');
             return redirect()->back();
         }
+        //*/
     }
     
     public function home() {
