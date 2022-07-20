a85bb7c699f32671a6e3bce9bfdbc4c87d5f6d02
inoue123456 <yuta1511814@gmail.com>
2022-07-20 13:11:56 +0000

    修正

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -6,11 +6,22 @@
     <div class="col-md-10 col-md-offset-2">
         <h5><a href="{{ route('personal_task.showProgress') }}">私の進行状況</a></h5>
         <h5>お知らせ</h5>
-        <ul>★</ul>
+        <ul>★新規業務依頼
+          @foreach($projects as $project)
+            @if($project->created_at <= date('Y-m-d H:i:s', time()-(3*24*60*60)))
+              <ul><a href={{ route('task.showProgress', $project)}}>{{ $project->name }}</a> 、納期{{ $project->deadline_date }}</ul>
+            @endif
+          @endforeach
+        </ul>
         <h5>担当プロジェクト</h5>
         @foreach($projects as $project)
-        <ul><a href={{ route('task.showProgress', $project)}}>{{ $project->name }}</a></ul>
+          <ul><a href={{ route('task.showProgress', $project)}}>{{ $project->name }}</a></ul>
         @endforeach
+        @auth
+          @if (auth()->user()->role >= 5)
+            <h5><a href="{{ route('project.index') }}">プロジェクト一覧</a></h5>
+          @endif
+        @endauth
     </div>
   </div>
 </div>
